03aa9b7d55e9aef823e49af3a2b0520dc7c021a4
octavianmetta <44347123+octavianmetta@users.noreply.github.com>
2018-10-22 22:42:30 +0700

    Tambahan union untuk convert float to byte

--- SlaveSender.ino
+++ SlaveSender.ino
@@ -1,14 +1,24 @@
 #include "DHT.h"
 #include <Wire.h>
-unsigned char I2Cdata[2];
-
+#define PACKET_SIZE 8
 #define DHTPIN 2     // what digital pin we're connected to
-
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
+const byte addrSlaveI2C =  8;  // I2C Slave address of this device
+float temp = 0;
+float humid = 0;
+
+byte I2C_Packet[PACKET_SIZE];            // Array to hold data sent over I2C to main Arduino
 DHT dht(DHTPIN, DHTTYPE);
 
+// convert float to byte array  source: http://mbed.org/forum/helloworld/topic/2053/
+typedef union float2bytes_t   // union consists of one variable represented in a number of different ways 
+{ 
+  float f; 
+  byte b[sizeof(float)]; 
+}; 
+
 void setup() {
-        Wire.begin(8);          // join i2c bus with address #8
+        Wire.begin(addrSlaveI2C);    // Initiate the Wire library and join the I2C bus         
         Wire.onRequest(RequestData);
         Serial.begin(9600);
         Serial.println("DHTxx test!");
@@ -19,15 +29,24 @@ void setup() {
 void loop() {
         delay(1000);
 
-        float h = dht.readHumidity();
-        float t = dht.readTemperature();
+        humid = dht.readHumidity();
+        temp = dht.readTemperature();
         // Check if any reads failed and exit early (to try again).
-        if (isnan(h) || isnan(t)) {
+        if (isnan(humid) || isnan(temp)) {
                 Serial.println("Failed to read from DHT sensor!");
                 return;
         }
-        I2Cdata[0] = t;
-        I2Cdata[1] = h;
+        float2bytes_t t2b,h2b; 
+        t2b.f = temp;
+        I2C_Packet[0] = t2b.b[0];
+        I2C_Packet[1] = t2b.b[1];
+        I2C_Packet[2] = t2b.b[2];
+        I2C_Packet[3] = t2b.b[3];
+        h2b.f = humid;
+        I2C_Packet[4] = h2b.b[0];
+        I2C_Packet[5] = h2b.b[1];
+        I2C_Packet[6] = h2b.b[2];
+        I2C_Packet[7] = h2b.b[3];
         Serial.print("Humidity: ");
         Serial.print(h);
         Serial.print(" %\t");
@@ -38,5 +57,5 @@ void loop() {
 
 void RequestData()
 {
-        Wire.write(I2Cdata,2);
+	Wire.write(I2C_Packet,PACKET_SIZE);
 }
